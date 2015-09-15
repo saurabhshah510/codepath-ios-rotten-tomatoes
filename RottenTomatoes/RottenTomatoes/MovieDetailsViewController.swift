84fb946868105a2ed794f88904ac3ed2beec133f
sshah1 <sshah0@walmartlabs.com>
2015-09-14 22:53:53 -0700

    Adding a dummy page for movie details and adding a navigation controller

---
+++ RottenTomatoes/RottenTomatoes/MovieDetailsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MovieDetailsViewController.swift
+//  RottenTomatoes
+//
+//  Created by Saurabh Shah on 9/14/15.
+//  Copyright © 2015 Saurabh Shah. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    var movie: NSDictionary!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

d59f361a4757ef42ba6da7fe62045105b70fcd98
sshah1 <sshah0@walmartlabs.com>
2015-09-14 19:26:28 -0700

    Ensuring that the rotten tomatoes API is called and that movie titles load

--- RottenTomatoes/RottenTomatoes/ViewController.swift
+++ RottenTomatoes/RottenTomatoes/ViewController.swift
@@ -12,10 +12,19 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     let CELL_NAME = "com.codepath.rottentomatoes.moviecell"
     @IBOutlet weak var movieTableView: UITableView!
+    var movies: [NSDictionary]? = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=dagqdghwaq3e3mxyrp7kmmj5")
+        let request = NSURLRequest(URL: url!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary
+            if responseDictionary != nil{
+                self.movies = responseDictionary!["movies"] as? [NSDictionary]
+                self.movieTableView.reloadData()
+            }            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,12 +33,19 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if self.movies != nil{
+            return self.movies!.count
+        }else{
+            return 0
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let movieCell = tableView.dequeueReusableCellWithIdentifier(CELL_NAME) as! MovieCell
-        movieCell.movieLabel.text = "Fast and Furious \(indexPath.row)"
+        if (self.movies != nil && self.movies!.count != 0) {
+            let movie = self.movies![indexPath.row]
+            movieCell.movieLabel.text = movie["title"] as? String
+        }
         return movieCell
     }
 
